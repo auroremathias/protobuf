0ce7521abccb19943e5d3551399943e13f972464
MATHIAS.OMNILOG, Aurore <a.mathias@tf1.onmicrosoft.com>
2026-02-12 14:24:50 +0100

    csharp : JsonFormatter can write Enum as strings

--- csharp/src/Google.Protobuf/JsonFormatter.cs
+++ csharp/src/Google.Protobuf/JsonFormatter.cs
@@ -346,29 +346,29 @@ namespace Google.Protobuf {
       };
     }
 
-    /// <summary>
-    /// Writes a single value to the given writer as JSON. Only types understood by
-    /// Protocol Buffers can be written in this way. This method is only exposed for
-    /// advanced use cases; most users should be using <see cref="Format(IMessage)"/>
-    /// or <see cref="Format(IMessage, TextWriter)"/>.
-    /// </summary>
-    /// <param name="writer">The writer to write the value to. Must not be null.</param>
-    /// <param name="value">The value to write. May be null.</param>
-    /// <remarks>Delegates to <c>WriteValue(TextWriter, object, int)</c> with <c>indentationLevel =
-    /// 0</c>.</remarks>
-    public void WriteValue(TextWriter writer, object value) => WriteValue(writer, value, 0);
+        /// <summary>
+        /// Writes a single value to the given writer as JSON. Only types understood by
+        /// Protocol Buffers can be written in this way. This method is only exposed for
+        /// advanced use cases; most users should be using <see cref="Format(IMessage)"/>
+        /// or <see cref="Format(IMessage, TextWriter)"/>.
+        /// </summary>
+        /// <param name="writer">The writer to write the value to. Must not be null.</param>
+        /// <param name="value">The value to write. May be null.</param>
+        /// <remarks>Delegates to <c>WriteValue(TextWriter, object, int)</c> with <c>indentationLevel =
+        /// 0</c>.</remarks>
+        public void WriteValue(TextWriter writer, object value) => WriteValue(writer, value, 0);
 
-    /// <summary>
-    /// Writes a single value to the given writer as JSON. Only types understood by
-    /// Protocol Buffers can be written in this way. This method is only exposed for
-    /// advanced use cases; most users should be using <see cref="Format(IMessage)"/>
-    /// or <see cref="Format(IMessage, TextWriter)"/>.
-    /// </summary>
-    /// <param name="writer">The writer to write the value to. Must not be null.</param>
-    /// <param name="value">The value to write. May be null.</param>
-    /// <param name="indentationLevel">The current indentationLevel. Not used when <see
-    /// cref="Settings.Indentation"/> is null.</param>
-    public void WriteValue(TextWriter writer, object value, int indentationLevel) {
+        /// <summary>
+        /// Writes a single value to the given writer as JSON. Only types understood by
+        /// Protocol Buffers can be written in this way. This method is only exposed for
+        /// advanced use cases; most users should be using <see cref="Format(IMessage)"/>
+        /// or <see cref="Format(IMessage, TextWriter)"/>.
+        /// </summary>
+        /// <param name="writer">The writer to write the value to. Must not be null.</param>
+        /// <param name="value">The value to write. May be null.</param>
+        /// <param name="indentationLevel">The current indentationLevel. Not used when <see
+        /// cref="Settings.Indentation"/> is null.</param>
+        public void WriteValue(TextWriter writer, object value, int indentationLevel) {
       if (value == null || value is NullValue) {
         WriteNull(writer);
       } else if (value is bool b) {
@@ -385,8 +385,8 @@ namespace Google.Protobuf {
       } else if (value is IList list) {
         WriteList(writer, list, indentationLevel);
       } else if (value is int || value is uint) {
-        IFormattable formattable = (IFormattable)value;
-        writer.Write(formattable.ToString("d", CultureInfo.InvariantCulture));
+          IFormattable formattable = (IFormattable)value;
+          writer.Write(formattable.ToString("d", CultureInfo.InvariantCulture));
       } else if (value is long || value is ulong) {
         writer.Write('"');
         IFormattable formattable = (IFormattable)value;
@@ -419,13 +419,13 @@ namespace Google.Protobuf {
       }
     }
 
-    /// <summary>
-    /// Central interception point for well-known type formatting. Any well-known types which
-    /// don't need special handling can fall back to WriteMessage. We avoid assuming that the
-    /// values are using the embedded well-known types, in order to allow for dynamic messages
-    /// in the future.
-    /// </summary>
-    private void WriteWellKnownTypeValue(TextWriter writer, MessageDescriptor descriptor,
+        /// <summary>
+        /// Central interception point for well-known type formatting. Any well-known types which
+        /// don't need special handling can fall back to WriteMessage. We avoid assuming that the
+        /// values are using the embedded well-known types, in order to allow for dynamic messages
+        /// in the future.
+        /// </summary>
+        private void WriteWellKnownTypeValue(TextWriter writer, MessageDescriptor descriptor,
                                          object value, int indentationLevel) {
       // Currently, we can never actually get here, because null values are always handled by the
       // caller. But if we *could*, this would do the right thing.
